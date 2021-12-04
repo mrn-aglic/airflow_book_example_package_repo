23486428b9ab4879e8786a37059260e92b54e23a
Marin Aglic <marin.aglic@kiwi.com>
2021-12-04 20:32:48 +0100

    rename package and ad init.py

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ import setuptools
 
 requirements = ["apache-airflow", "requests"]
 
+extra_requirements = {"dev": ["pytest"]}
+
 setuptools.setup(
-    name="airflow_movielens",
+    name="airflowbook",
     version="0.1.0",
     description="Hooks, sensors and operators for the Movielens API.",
     author="Anonymous",
     author_email="anonymous@example.com",
     install_requires=requirements,
+    extras_require=extra_requirements,
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/mrn-aglic/airflow_book_example_package_repo",
