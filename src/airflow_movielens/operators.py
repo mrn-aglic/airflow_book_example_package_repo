cfbe5cfc801da26ad80b027d7527950ad51c49f9
Marin Aglic <marin.aglic@kiwi.com>
2021-12-04 19:35:11 +0100

    Added ch09 MovielensPopularityOperator

--- src/airflow_movielens/operators.py
+++ src/airflow_movielens/operators.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict, Counter
 from pathlib import Path
 from typing import Any
 
@@ -30,12 +31,12 @@ class MovielensFetchRatingsOperators(BaseOperator):
     template_fields = ["_start_date", "_end_date", "_output_path"]
 
     def __init__(
-        self,
-        conn_id,
-        output_path,
-        start_date="{{ds}}",
-        end_date="{{next_ds}}",
-        **kwargs,
+            self,
+            conn_id,
+            output_path,
+            start_date="{{ds}}",
+            end_date="{{next_ds}}",
+            **kwargs,
     ):
         super().__init__(**kwargs)
 
@@ -67,3 +68,34 @@ class MovielensFetchRatingsOperators(BaseOperator):
 
         with open(self._output_path, "w") as file:
             json.dump(ratings, file)
+
+
+# This operator is introduced in chapter 09 of the book Data Pipelines with Apache Airflow
+class MovielensPopularityOperator(BaseOperator):
+
+    def __init__(self, conn_id, start_date, end_date, min_ratings=4, top_n=5, **kwargs):
+        super().__init__(**kwargs)
+        self._conn_id = conn_id
+        self._start_date = start_date
+        self._end_date = end_date
+        self._min_ratings = min_ratings
+        self._top_n = top_n
+
+    def execute(self, context: Any):
+        with MovielensHook(self._conn_id) as hook:
+            ratings = hook.get_ratings(
+                start_date=self._start_date,
+                end_date=self._end_date
+            )
+
+            rating_sums = defaultdict(Counter)
+            for rating in ratings:
+                rating_sums[rating["movieId"]].update(count=1, rating=rating["rating"])
+
+            averages = {
+                movie_id: (rating_counter["rating"] / rating_counter["count"], rating_counter["count"])
+                for movie_id, rating_counter in rating_sums.items()
+                if rating_counter["count"] >= self._min_ratings
+            }
+
+            return sorted(averages.items(), key=lambda x: x[1], reverse=True)[:self._top_n]
